972d1359092c1ede74dfc076b47354a01be94204
mirielimacedo <mirielimacedo@gmail.com>
2024-01-09 11:59:50 +0800

    Updated time zone

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-function updateAdelaideTime(){
+function updateTime(){
 //Adelaide
 let adelaideElement = document.querySelector("#adelaide");
+if(adelaideElement){
 let adelaideDateElement =  adelaideElement.querySelector(".date");
 let adelaideTimeElement = adelaideElement.querySelector(".time");
 let adelaideTime = moment().tz("Australia/Adelaide");
@@ -8,18 +9,42 @@ let adelaideTime = moment().tz("Australia/Adelaide");
 adelaideDateElement.innerHTML= adelaideTime.format("MMMM Do YYYY");
 adelaideTimeElement.innerHTML =  adelaideTime.format("h:mm:ss [<small>]A[</small>]" );
 }
-updateAdelaideTime();
-setInterval(updateAdelaideTime, 1000);
+}
 
-function updatePerthTime(){
 //Perth
 let perthElement = document.querySelector("#perth");
+if(perthElement){
 let perthDateElement =  perthElement.querySelector(".date");
 let perthTimeElement = perthElement.querySelector(".time");
 let perthTime = moment().tz("Australia/Perth");
 
 perthDateElement.innerHTML= perthTime.format("MMMM Do YYYY");
 perthTimeElement.innerHTML =  perthTime.format("h:mm:ss [<small>]A[</small>]" );
+
 }
-updatePerthTime();
-setInterval(updatePerthTime, 1000);
+
+function updateCity(event){
+     let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment().tz().guess();
+  }
+  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
+  let cityTime = moment().tz(cityTimeZone);
+  let citiesElement = document.querySelector("#cities");
+  citiesElement.innerHTML = `
+  <div class="city">
+    <div>
+      <h2>${cityName}</h2>
+      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    </div>
+    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
+    "A"
+  )}</small></div>
+  </div>
+  `;
+}
+updateTime();
+setInterval(updateTime, 1000);
+
+let citiesSelectElement = document.querySelector("#city");
+citiesSelectElement.addEventListener("change",updateCity);
