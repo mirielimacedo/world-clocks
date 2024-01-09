2e58d602bcdb242c9d8236b3d0f13ccd1f726d65
mirielimacedo <mirielimacedo@gmail.com>
2024-01-09 13:42:08 +0800

    Update time zone for texas and cuiaba

--- index.js
+++ index.js
@@ -8,41 +8,44 @@ let adelaideTime = moment().tz("Australia/Adelaide");
 
 adelaideDateElement.innerHTML= adelaideTime.format("MMMM Do YYYY");
 adelaideTimeElement.innerHTML =  adelaideTime.format("h:mm:ss [<small>]A[</small>]" );
+
+//Texas
+let texasElement = document.querySelector("#texas");
+if(texasElement){
+let texasDateElement =  texasElement.querySelector(".date");
+let texasTimeElement = texasElement.querySelector(".time");
+let texasTime = moment().tz("America/Texas");
+
+texasDateElement.innerHTML= texasTime.format("MMMM Do YYYY");
+texasTimeElement.innerHTML =  texasTime.format("h:mm:ss [<small>]A[</small>]" );
+
+}
 }
 }
 
-//Perth
-let perthElement = document.querySelector("#perth");
-if(perthElement){
-let perthDateElement =  perthElement.querySelector(".date");
-let perthTimeElement = perthElement.querySelector(".time");
-let perthTime = moment().tz("Australia/Perth");
 
-perthDateElement.innerHTML= perthTime.format("MMMM Do YYYY");
-perthTimeElement.innerHTML =  perthTime.format("h:mm:ss [<small>]A[</small>]" );
-
-}
 
 function updateCity(event){
-     let cityTimeZone = event.target.value;
+  let cityTimeZone = event.target.value;
   if (cityTimeZone === "current") {
-    cityTimeZone = moment().tz().guess();
+    cityTimeZone = moment.tz.guess();
   }
-  let cityName = cityTimeZone.replace("_", " ").split("/")[1];
-  let cityTime = moment().tz(cityTimeZone);
+
+  let cityName = cityTimeZone.split("/")[1];
+  let cityTime = moment.tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
+
   citiesElement.innerHTML = `
-  <div class="city">
-    <div>
-      <h2>${cityName}</h2>
-      <div class="date">${cityTime.format("MMMM	Do YYYY")}</div>
+    <div class="city">
+      <div>
+        <h2>${cityName}</h2>
+        <div class="date">${cityTime.format("MMMM Do YYYY")}</div>
+      </div>
+      <div class="time">${cityTime.format("h:mm:ss A")}</div>
     </div>
-    <div class="time">${cityTime.format("h:mm:ss")} <small>${cityTime.format(
-    "A"
-  )}</small></div>
-  </div>
   `;
 }
+
 updateTime();
 setInterval(updateTime, 1000);
 
